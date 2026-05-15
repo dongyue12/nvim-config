516f9623ad658694f76595898920accd7f9bfc79
vali <yzrzno@163.com>
2026-05-15 21:12:54 +0800

    feat: 提高选中文本高亮对比度

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,6 @@
 return {
   { "folke/tokyonight.nvim", priority = 1000, config = function()
     vim.cmd.colorscheme("tokyonight-storm")
+    vim.api.nvim_set_hl(0, "Visual", { bg = "#3b4261", fg = "NONE" })
   end },
 }

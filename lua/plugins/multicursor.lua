4629b2a5c712db5845608f3cb0a64faf82cd9b31
vali <yzrzno@163.com>
2026-05-15 20:41:59 +0800

    fix: 修正多光标映射为 VM-Find-Under

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -2,7 +2,7 @@ return {
   "mg979/vim-visual-multi",
   lazy = false,
   config = function()
-    vim.keymap.set("n", "<C-d>", "<Plug>(VM-Add-Cursor-Down)")
-    vim.keymap.set("x", "<C-d>", "<Plug>(VM-Add-Cursor-Down)")
+    vim.keymap.set("n", "<C-d>", "<Plug>(VM-Find-Under)")
+    vim.keymap.set("x", "<C-d>", "<Plug>(VM-Find-Subword-Under)")
   end,
 }
